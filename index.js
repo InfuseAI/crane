614738982707d33686b65590d580d64511b680fc
Kent Huang <kentwelcome@gmail.com>
2021-09-23 17:41:08 +0800

    [Update] remove unused options
    Signed-off-by: Kent Huang <kentwelcome@gmail.com>

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ function createWindow(socketName) {
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
+      nodeIntegration: false,
       preload: __dirname + '/client-preload.js'
     }
   })
