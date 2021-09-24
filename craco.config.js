1f0e470c96ca5f8f472f114548dfc06ecf2b723c
Aaron Huang <aaroms9733@gmail.com>
2021-09-25 00:36:13 +0800

    Clean packing script
    Signed-off-by: Aaron Huang <aaroms9733@gmail.com>

--- craco.config.js
+++ craco.config.js
@@ -1,1 +1,22 @@
-module.exports = {};
+const WebpackBar = require('webpackbar');
+const SimpleProgressWebpackPlugin = require('simple-progress-webpack-plugin');
+const path = require('path');
+const pathResolve = (pathUrl) => path.join(__dirname, pathUrl);
+module.exports = {
+  webpack: {
+    alias: {
+      '@': pathResolve('.'),
+      src: pathResolve('src'),
+      common: pathResolve('src/common'),
+      components: pathResolve('src/components'),
+      hooks: pathResolve('src/hooks'),
+      utils: pathResolve('src/utils'),
+    },
+    plugins: [
+      new WebpackBar(),
+      new SimpleProgressWebpackPlugin({
+        format: 'compact',
+      }),
+    ],
+  },
+};
