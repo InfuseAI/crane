fe3ae27230469052f8f35192d297118b26af2ef8
Aaron Huang <aaroms9733@gmail.com>
2021-09-25 00:56:56 +0800

    Add circular dependency detector
    Signed-off-by: Aaron Huang <aaroms9733@gmail.com>

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,7 @@
+const { whenDev } = require('@craco/craco');
 const WebpackBar = require('webpackbar');
 const SimpleProgressWebpackPlugin = require('simple-progress-webpack-plugin');
+const CircularDependencyPlugin = require('circular-dependency-plugin');
 const path = require('path');
 const pathResolve = (pathUrl) => path.join(__dirname, pathUrl);
 module.exports = {
@@ -17,6 +19,15 @@ module.exports = {
       new SimpleProgressWebpackPlugin({
         format: 'compact',
       }),
+      ...whenDev(() => [
+        new CircularDependencyPlugin({
+          exclude: /node_modules/,
+          include: /src/,
+          failOnError: true,
+          allowAsyncCycles: false,
+          cwd: process.cwd(),
+        }),
+      ]),
     ],
   },
 };
