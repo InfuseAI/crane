c43e078a4c6d9248fdd3a588207cfcbabc0b669f
Aaron Huang <aaroms9733@gmail.com>
2021-09-24 18:50:01 +0800

    prettier fix
    Signed-off-by: Aaron Huang <aaroms9733@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import React, { Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import './App.css';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import "./App.css";
+import { Layout, Menu, Breadcrumb } from "antd";
 import {
   OrderedListOutlined,
   PlusOutlined,
-  SettingOutlined
-} from '@ant-design/icons';
-const BuildImage = lazy(() => import('./BuildImage'));
+  SettingOutlined,
+} from "@ant-design/icons";
+const BuildImage = lazy(() => import("./BuildImage"));
 const { Content, Footer, Sider } = Layout;
 
 class Crane extends React.Component {
@@ -15,7 +15,7 @@ class Crane extends React.Component {
     collapsed: false,
   };
 
-  onCollapse = collapsed => {
+  onCollapse = (collapsed) => {
     console.log(collapsed);
     this.setState({ collapsed });
   };
@@ -23,18 +23,27 @@ class Crane extends React.Component {
   render() {
     const { collapsed } = this.state;
     return (
-      <Layout style={{ minHeight: '100vh' }}>
+      <Layout style={{ minHeight: "100vh" }}>
         <Router>
           <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse}>
             <div className="logo" />
-            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" style={{position: 'relative', height: 'calc(100% - 32px)'}}>
+            <Menu
+              theme="dark"
+              defaultSelectedKeys={["1"]}
+              mode="inline"
+              style={{ position: "relative", height: "calc(100% - 32px)" }}
+            >
               <Menu.Item key="1" icon={<PlusOutlined />}>
-                <Link to='/'>Build a new Image</Link>
+                <Link to="/">Build a new Image</Link>
               </Menu.Item>
               <Menu.Item key="2" icon={<OrderedListOutlined />}>
-                <Link to='/images'>Images</Link>
+                <Link to="/images">Images</Link>
               </Menu.Item>
-              <Menu.Item style={{position: 'relative', top: "calc(100% - 172px)"}} key="3" icon={<SettingOutlined />}>
+              <Menu.Item
+                style={{ position: "relative", top: "calc(100% - 172px)" }}
+                key="3"
+                icon={<SettingOutlined />}
+              >
                 Settings
               </Menu.Item>
             </Menu>
@@ -42,22 +51,26 @@ class Crane extends React.Component {
           <Layout className="site-layout">
             <Suspense fallback={<div>Loading...</div>}>
               <Switch>
-                <Route exact path='/' component={BuildImage}>
-                </Route>
-                <Route path='/images'>
-                  <Content style={{ margin: '0 16px' }}>
-                    <Breadcrumb style={{ margin: '16px 0' }}>
+                <Route exact path="/" component={BuildImage}></Route>
+                <Route path="/images">
+                  <Content style={{ margin: "0 16px" }}>
+                    <Breadcrumb style={{ margin: "16px 0" }}>
                       <Breadcrumb.Item>Crane</Breadcrumb.Item>
                       <Breadcrumb.Item>Images</Breadcrumb.Item>
                     </Breadcrumb>
-                    <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+                    <div
+                      className="site-layout-background"
+                      style={{ padding: 24, minHeight: 360 }}
+                    >
                       Image List
                     </div>
                   </Content>
                 </Route>
               </Switch>
             </Suspense>
-            <Footer style={{ textAlign: 'center' }}>Crane ©2021 Created by InfuseAI</Footer>
+            <Footer style={{ textAlign: "center" }}>
+              Crane ©2021 Created by InfuseAI
+            </Footer>
           </Layout>
         </Router>
       </Layout>
