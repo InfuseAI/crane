85b7be5aa0e9e3674ef0a79d0985119a4f4c9510
Aaron Huang <aaroms9733@gmail.com>
2021-09-24 18:55:57 +0800

    Prettier
    Signed-off-by: Aaron Huang <aaroms9733@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import React, { Suspense, lazy } from "react";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import "./App.css";
-import { Layout, Menu, Breadcrumb } from "antd";
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import './App.css';
+import { Layout, Menu, Breadcrumb } from 'antd';
 import {
   OrderedListOutlined,
   PlusOutlined,
   SettingOutlined,
-} from "@ant-design/icons";
-const BuildImage = lazy(() => import("./BuildImage"));
+} from '@ant-design/icons';
+const BuildImage = lazy(() => import('./BuildImage'));
 const { Content, Footer, Sider } = Layout;
 
 class Crane extends React.Component {
@@ -15,7 +15,7 @@ class Crane extends React.Component {
     collapsed: false,
   };
 
-  onCollapse = (collapsed) => {
+  onCollapse = collapsed => {
     console.log(collapsed);
     this.setState({ collapsed });
   };
@@ -23,43 +23,43 @@ class Crane extends React.Component {
   render() {
     const { collapsed } = this.state;
     return (
-      <Layout style={{ minHeight: "100vh" }}>
+      <Layout style={{ minHeight: '100vh' }}>
         <Router>
           <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse}>
-            <div className="logo" />
+            <div className='logo' />
             <Menu
-              theme="dark"
-              defaultSelectedKeys={["1"]}
-              mode="inline"
-              style={{ position: "relative", height: "calc(100% - 32px)" }}
+              theme='dark'
+              defaultSelectedKeys={['1']}
+              mode='inline'
+              style={{ position: 'relative', height: 'calc(100% - 32px)' }}
             >
-              <Menu.Item key="1" icon={<PlusOutlined />}>
-                <Link to="/">Build a new Image</Link>
+              <Menu.Item key='1' icon={<PlusOutlined />}>
+                <Link to='/'>Build a new Image</Link>
               </Menu.Item>
-              <Menu.Item key="2" icon={<OrderedListOutlined />}>
-                <Link to="/images">Images</Link>
+              <Menu.Item key='2' icon={<OrderedListOutlined />}>
+                <Link to='/images'>Images</Link>
               </Menu.Item>
               <Menu.Item
-                style={{ position: "relative", top: "calc(100% - 172px)" }}
-                key="3"
+                style={{ position: 'relative', top: 'calc(100% - 172px)' }}
+                key='3'
                 icon={<SettingOutlined />}
               >
                 Settings
               </Menu.Item>
             </Menu>
           </Sider>
-          <Layout className="site-layout">
+          <Layout className='site-layout'>
             <Suspense fallback={<div>Loading...</div>}>
               <Switch>
-                <Route exact path="/" component={BuildImage}></Route>
-                <Route path="/images">
-                  <Content style={{ margin: "0 16px" }}>
-                    <Breadcrumb style={{ margin: "16px 0" }}>
+                <Route exact path='/' component={BuildImage}></Route>
+                <Route path='/images'>
+                  <Content style={{ margin: '0 16px' }}>
+                    <Breadcrumb style={{ margin: '16px 0' }}>
                       <Breadcrumb.Item>Crane</Breadcrumb.Item>
                       <Breadcrumb.Item>Images</Breadcrumb.Item>
                     </Breadcrumb>
                     <div
-                      className="site-layout-background"
+                      className='site-layout-background'
                       style={{ padding: 24, minHeight: 360 }}
                     >
                       Image List
@@ -68,7 +68,7 @@ class Crane extends React.Component {
                 </Route>
               </Switch>
             </Suspense>
-            <Footer style={{ textAlign: "center" }}>
+            <Footer style={{ textAlign: 'center' }}>
               Crane ©2021 Created by InfuseAI
             </Footer>
           </Layout>
@@ -79,7 +79,7 @@ class Crane extends React.Component {
 }
 
 const App = () => (
-  <div id="crane" className="App">
+  <div id='crane' className='App'>
     <Crane />
   </div>
 );
--- src/BuildImage.jsx
+++ src/BuildImage.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import { Layout, Breadcrumb } from "antd";
+import React from 'react';
+import { Layout, Breadcrumb } from 'antd';
 const { Content } = Layout;
 
 export default function BuildImage() {
   return (
-    <Content style={{ margin: "0 16px" }}>
-      <Breadcrumb style={{ margin: "16px 0" }}>
+    <Content style={{ margin: '0 16px' }}>
+      <Breadcrumb style={{ margin: '16px 0' }}>
         <Breadcrumb.Item>Crane</Breadcrumb.Item>
         <Breadcrumb.Item>Build a new Image</Breadcrumb.Item>
       </Breadcrumb>
       <div
-        className="site-layout-background"
+        className='site-layout-background'
         style={{ padding: 24, minHeight: 360 }}
       >
         Builder Form
