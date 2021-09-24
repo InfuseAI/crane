1bb571a65396e0ad41b70c2253ad84082d9a2bc3
Ash Wu <hSATAC@gmail.com>
2021-09-24 11:11:59 +0800

    Generate dockerfile and build image
    Signed-off-by: Ash Wu <hSATAC@gmail.com>
    Co-Authored-By: Kent Huang <kent@infuseai.io>

--- server-handlers.js
+++ server-handlers.js
@@ -1,12 +1,52 @@
 const Docker = require('dockerode');
 const opts = require('./server-opts')
 const docker = new Docker();
+const fs = require('fs');
+const path = require('path');
 
 let handlers = {}
 
-handlers["build-image"] = async ({ base_image_url, apt, conda, pip }) => {
-  console.log(opts.workingDir);
+function generateDockerfile(options) {
+  let base_image_url = options['base_image_url'];
+  let apt = ''
+  let conda = ''
+  let pip = ''
 
+  if (options['apt']) {
+    apt = `RUN apt-get update && apt-get install -y --no-install-recommends ${options['apt'].replace(/[\r\n]+/g, ' ')} && apt-get purge && apt-get clean`
+  }
+
+  if (options['conda']) {
+    conda = `RUN conda install --quiet --yes ${options['conda'].replaceAll(/[\r\n]+/g, ' ')} && conda clean --all -f -y`
+  }
+
+  if (options['pip']) {
+    pip = `RUN pip install --no-cache-dir ${options['pip'].replaceAll(/[\r\n]+/g, ' ')}`
+  }
+
+  let dockerfileContent = `FROM ${base_image_url}
+USER root
+${apt}
+${conda}
+${pip}`
+
+  return dockerfileContent
+}
+
+function writeDockerfile(dockerfileContent) {
+  fs.writeFileSync(path.join(opts.workingDir, 'Dockerfile'), dockerfileContent);
+}
+
+handlers["build-image"] = async ({ base_image_url, apt, conda, pip }) => {
+  console.log("build-image");
+  writeDockerfile(generateDockerfile({base_image_url, apt, conda, pip}));
+  docker.buildImage({
+    context: opts.workingDir,
+    src: ['Dockerfile']
+  }, {t: 'cranetest'}, function (err, response) {
+    console.log(err)
+    console.log(response)
+  });
   return "Start building"
 }
 
