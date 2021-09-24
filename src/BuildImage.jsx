b341aa070d1012640d09f2628384826dfb0da6fa
Aaron Huang <aaroms9733@gmail.com>
2021-09-24 18:49:02 +0800

    Update client script
    Signed-off-by: Aaron Huang <aaroms9733@gmail.com>

---
+++ src/BuildImage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Layout, Breadcrumb } from 'antd';
+const { Content } = Layout;
+
+export default function BuildImage() {
+  return (
+    <Content style={{ margin: '0 16px' }}>
+      <Breadcrumb style={{ margin: '16px 0' }}>
+        <Breadcrumb.Item>Crane</Breadcrumb.Item>
+        <Breadcrumb.Item>Build a new Image</Breadcrumb.Item>
+      </Breadcrumb>
+      <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+        Builder Form
+      </div>
+    </Content>
+  );
+};
