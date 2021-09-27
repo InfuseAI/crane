99128ebc1c4d0bf8021a96a99419e93295db9ec0
Kent Huang <kentwelcome@gmail.com>
2021-09-27 16:01:10 +0800

    [Feature] stream build log to front-end
    Co-authored-by: Ash Wu <hsatac@infuseai.io>
    Signed-off-by: Kent Huang <kentwelcome@gmail.com>

--- public/server-handlers.js
+++ public/server-handlers.js
@@ -4,7 +4,7 @@ const docker = new Docker();
 const fs = require('fs');
 const path = require('path');
 const keytar = require('keytar');
-
+const { send } = require('./server-ipc');
 let handlers = {}
 
 handlers.build_events = []
@@ -63,10 +63,10 @@ function writeDockerfile(dockerfileContent) {
 handlers["get-dockerhub-credential"] = getDockerHubCredential;
 handlers["save-dockerhub-credential"] = async (args) => await saveDockerHubCredential(args.account, args.password);
 
-handlers["build-image"] = async ({ base_image_url, apt, conda, pip }) => {
+handlers["build-image"] = async ({ base_image_url, image_name, apt, conda, pip }) => {
   handlers.build_events = []
   handlers.build_status = 'preparing'
-  let image_name = 'infuseaidev/cranetest:latest'
+  // let image_name = 'infuseaidev/cranetest:latest'
 
   console.log("build-image", base_image_url, apt, conda, pip);
 
@@ -78,34 +78,38 @@ handlers["build-image"] = async ({ base_image_url, apt, conda, pip }) => {
 
   docker.modem.followProgress(build_stream, buildFinished, (event) => {
     console.log(event);
+    send('build-log', event);
     handlers.build_events.push(event)
     handlers.build_status = 'building'
   });
 
   async function buildFinished(err, output) {
-    const credential = await getDockerHubCredential();
-    if (credential) {
-      const auth = {
-        username: credential.account,
-        password: credential.password,
-        serveraddress: 'https://index.docker.io/v1'
-      };
-      const push_stream = await docker.getImage(image_name).push({'authconfig': auth})
-      docker.modem.followProgress(push_stream, (err, output) => {
-        handlers.build_status = 'finished'
-        console.log('push finished', err, output)
-      }, (event) => {
-        handlers.build_status = 'pushing'
-        console.log(event);
-        handlers.build_events.push(event)
-      });
-    } else {
-      console.log('No Credential');
-    }
+    console.log('Build finished', err, output);
   }
-
-  
-  return "Start building"
+  return "Start building";
 }
+
+handlers["push-image-dockerhub"] = async ({ image_name }) => {
+  const credential = await getDockerHubCredential();
+  if (credential) {
+    const auth = {
+      username: credential.account,
+      password: credential.password,
+      serveraddress: 'https://index.docker.io/v1'
+    };
+    const push_stream = await docker.getImage(image_name).push({'authconfig': auth})
+    docker.modem.followProgress(push_stream, (err, output) => {
+      handlers.build_status = 'finished'
+      console.log('push finished', err, output);
+    }, (event) => {
+      console.log(event);
+      send('push-log', event);
+      handlers.build_events.push(event);
+    });
+  } else {
+      console.log('No Credential');
+  }
+  return "Start pushing";
+};
 
 module.exports = handlers
