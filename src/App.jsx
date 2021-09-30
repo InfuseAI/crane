2c8878f34fd28fc725119d684f6e6e3117699e3e
Aaron Huang <aaroms9733@gmail.com>
2021-09-30 20:49:12 +0800

    Extract Sidebar component
    - also remove setTimeout of margeLeft setting.
    
    Signed-off-by: Aaron Huang <aaroms9733@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,101 +1,28 @@
-import React, { Suspense, lazy, useState } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Link,
-  useLocation,
-} from 'react-router-dom';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.less';
-import { Layout, Menu, Skeleton } from 'antd';
-import {
-  OrderedListOutlined,
-  PlusOutlined,
-  SettingOutlined,
-} from '@ant-design/icons';
+import { Layout, Skeleton } from 'antd';
+import Sidebar from './Sidebar';
 
 const BuildImage = lazy(() => import('./BuildImage'));
 const ListImage = lazy(() => import('./ListImage'));
 const Settings = lazy(() => import('./Settings'));
-const { Sider, Footer } = Layout;
-
-const CraneMenu = () => {
-  const location = useLocation();
-  console.log('pathname:', location.pathname);
-  return (
-    <Menu
-      theme='dark'
-      defaultSelectedKeys={[location.pathname.replaceAll('/', '')]}
-      selectedKeys={[location.pathname.replaceAll('/', '')]}
-      mode='inline'
-      style={{ position: 'relative' }}
-    >
-      <Menu.Item key='' icon={<PlusOutlined />}>
-        <Link to='/' style={{ color: 'white' }}>
-          Build a new Image
-        </Link>
-      </Menu.Item>
-      <Menu.Item key='images' icon={<OrderedListOutlined />}>
-        <Link to='/images' style={{ color: 'white' }}>
-          Images
-        </Link>
-      </Menu.Item>
-      <Menu.Item
-        style={{ position: 'relative', top: 'max(0px, 100vh - 252px)' }}
-        key='settings'
-        icon={<SettingOutlined />}
-      >
-        <Link to='/settings' style={{ color: 'white' }}>
-          Settings
-        </Link>
-      </Menu.Item>
-    </Menu>
-  );
-};
+const { Footer } = Layout;
 
 const Crane = () => {
-  const [collapsed, setCollapsed] = useState(true);
   const siderWidth = {
     collapsed: 80,
     unCollapsed: 190,
   };
-  const [marginLeft, updateMarginLeft] = useState(siderWidth.collapsed);
 
-  const onCollapse = (collapsed) => {
-    setCollapsed(collapsed);
-    // Make the collapse animation smoother
-    setTimeout(
-      () => {
-        updateMarginLeft(
-          collapsed ? siderWidth.collapsed : siderWidth.unCollapsed
-        );
-      },
-      collapsed ? 100 : 0
-    );
-  };
   return (
     <Layout
       style={{
         minHeight: '100vh',
-        marginLeft: marginLeft,
       }}
     >
       <Router>
-        <Sider
-          collapsible
-          collapsed={collapsed}
-          onCollapse={onCollapse}
-          width={siderWidth.unCollapsed}
-          style={{
-            overflow: 'auto',
-            height: '100vh',
-            position: 'fixed',
-            left: 0,
-          }}
-        >
-          <div className='logo' />
-          <CraneMenu />
-        </Sider>
+        <Sidebar collapsed={true} width={siderWidth.unCollapsed} />
         <Layout className='site-layout'>
           <Suspense
             fallback={
@@ -111,7 +38,13 @@ const Crane = () => {
             </Switch>
           </Suspense>
           <Footer style={{ textAlign: 'center' }}>
-            <div>Crane, an <a href='https://infuseai.io' target='_blank' rel='noreferrer'>InfuseAI</a> product.</div>
+            <div>
+              Crane, an{' '}
+              <a href='https://infuseai.io' target='_blank' rel='noreferrer'>
+                InfuseAI
+              </a>{' '}
+              product.
+            </div>
             <div>Made with Love. ❤️ </div>
           </Footer>
         </Layout>
---
+++ src/Sidebar.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { Menu } from 'antd';
+import { Layout } from 'antd';
+import {
+  OrderedListOutlined,
+  PlusOutlined,
+  SettingOutlined,
+} from '@ant-design/icons';
+const { Sider } = Layout;
+export default function Sidebar(prop) {
+  const [collapsed, setCollapsed] = useState(prop.collapsed);
+  const { onCollapse, width } = prop;
+  const handleCollapse = (collapsed) => {
+    setCollapsed(collapsed);
+    if (onCollapse) {
+      onCollapse(collapsed);
+    }
+  };
+  const location = useLocation();
+  return (
+    <Sider
+      collapsible
+      collapsed={collapsed}
+      onCollapse={handleCollapse}
+      width={width}
+      style={{
+        overflow: 'auto',
+        height: '100vh',
+        position: 'sticky',
+        left: 0,
+        top: 0,
+      }}
+    >
+      <div className='logo' />
+      <Menu
+        theme='dark'
+        defaultSelectedKeys={[location.pathname.replaceAll('/', '')]}
+        selectedKeys={[location.pathname.replaceAll('/', '')]}
+        mode='inline'
+        style={{ position: 'relative' }}
+      >
+        <Menu.Item key='' icon={<PlusOutlined />}>
+          <Link to='/' style={{ color: 'white' }}>
+            Build
+          </Link>
+        </Menu.Item>
+        <Menu.Item key='images' icon={<OrderedListOutlined />}>
+          <Link to='/images' style={{ color: 'white' }}>
+            Images
+          </Link>
+        </Menu.Item>
+        <Menu.Item
+          style={{ position: 'relative', top: 'max(0px, 100vh - 252px)' }}
+          key='settings'
+          icon={<SettingOutlined />}
+        >
+          <Link to='/settings' style={{ color: 'white' }}>
+            Settings
+          </Link>
+        </Menu.Item>
+      </Menu>
+    </Sider>
+  );
+}
