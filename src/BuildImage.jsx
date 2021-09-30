c694dd2169f544c084384b5f9a56a9c0f68c6639
Aaron Huang <aaroms9733@gmail.com>
2021-09-30 15:27:49 +0800

    move to list after build
    Signed-off-by: Aaron Huang <aaroms9733@gmail.com>

--- src/BuildImage.jsx
+++ src/BuildImage.jsx
@@ -15,6 +15,7 @@ import useLocalStorage from './hooks/useLocalStorage';
 import { SiPython } from 'react-icons/si';
 import { send, listen, unlisten } from './utils/ipcClient';
 import { LazyLog, ScrollFollow } from 'react-lazylog';
+import { useHistory } from 'react-router-dom';
 
 const { Content } = Layout;
 const { TextArea } = Input;
@@ -28,58 +29,59 @@ const Status = {
   PROGRESSING: 'progressing',
 };
 
-const buildNotification = (name, isSuccess) => {
-  if (isSuccess) {
-    notification['success']({
-      message: 'Build Success',
-      description: `Image ${name || ''} is ready`,
-    });
-  } else {
-    notification['error']({
-      message: 'Build Failed',
-      description: `Image ${name || ''} failed`,
-    });
-  }
-};
-
-const renderTitle = (title) => (
-  <span>
-    {title}
-    <a
-      style={{ float: 'right' }}
-      href='https://docs.primehub.io/docs/guide_manual/images-list'
-      target='_blank'
-      rel='noopener noreferrer'
-    >
-      details
-    </a>
-  </span>
-);
-
-const renderItem = (imageName, pythonVersion) => ({
-  value: imageName,
-  label: (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'space-between',
-      }}
-    >
-      {imageName}
-      <span>
-        {<SiPython />} {pythonVersion}
-      </span>
-    </div>
-  ),
-});
-
 export default function BuildImage() {
+  const history = useHistory();
   const [options, updateOptions] = useState([]);
   const [logDrawerVisible, setLogDrawerVisible] = useState(false);
   const [blockBuildButton, setBlockBuildButton] = useState(false);
   const [logText, setLogText] = useLocalStorage('build_log');
   const [form] = Form.useForm();
   const placeholder = `one package per line. e.g., \npackage1\npackage2\n`;
+  const buildNotification = (name, isSuccess) => {
+    if (isSuccess) {
+      notification['success']({
+        message: 'Build Success',
+        description: `Image ${name || ''} is ready`,
+      });
+      history.push('/images');
+    } else {
+      notification['error']({
+        message: 'Build Failed',
+        description: `Image ${name || ''} failed`,
+      });
+    }
+  };
+
+  const renderTitle = (title) => (
+    <span>
+      {title}
+      <a
+        style={{ float: 'right' }}
+        href='https://docs.primehub.io/docs/guide_manual/images-list'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        details
+      </a>
+    </span>
+  );
+
+  const renderItem = (imageName, pythonVersion) => ({
+    value: imageName,
+    label: (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+        }}
+      >
+        {imageName}
+        <span>
+          {<SiPython />} {pythonVersion}
+        </span>
+      </div>
+    ),
+  });
   const onFinish = async (values) => {
     setBlockBuildButton(true);
     setLogDrawerVisible(true);
@@ -92,21 +94,27 @@ export default function BuildImage() {
   const onCloseLogDrawer = () => {
     setLogDrawerVisible(false);
   };
-  const buildLogReceiver = useCallback((payload) => {
-    if (payload.stage === Status.FINISHED) {
-      const name = payload.name;
-      buildNotification(name, !payload.output.find((x) => x.error));
-      setBlockBuildButton(false);
-      unlisten('build-log');
-    } else if (payload.stage === Status.PROGRESSING) {
-      if (payload.output.stream) {
-        setLogText((prevData) => prevData + payload.output.stream);
-      } else  if (payload.output.progress) {
-        // If has progress replace last line make progress bar like animation
-        setLogText((prevData) => prevData.replace(/\n.*$/, '\n') + payload.output.progress);
+  const buildLogReceiver = useCallback(
+    (payload) => {
+      if (payload.stage === Status.FINISHED) {
+        const name = payload.name;
+        buildNotification(name, !payload.output.find((x) => x.error));
+        setBlockBuildButton(false);
+        unlisten('build-log');
+      } else if (payload.stage === Status.PROGRESSING) {
+        if (payload.output.stream) {
+          setLogText((prevData) => prevData + payload.output.stream);
+        } else if (payload.output.progress) {
+          // If has progress replace last line make progress bar like animation
+          setLogText(
+            (prevData) =>
+              prevData.replace(/\n.*$/, '\n') + payload.output.progress
+          );
+        }
       }
-    }
-  }, [setLogText]);
+    },
+    [setLogText]
+  );
 
   const initialValues = {
     base_image_url: 'ubuntu:xenial',
@@ -167,7 +175,7 @@ export default function BuildImage() {
           onFinish={onFinish}
         >
           <Form.Item label='Image Name' name='image_name' required>
-            <Input disabled={blockBuildButton}/>
+            <Input disabled={blockBuildButton} />
           </Form.Item>
           <Form.Item label='Base Image' name='base_image_url' required>
             <AutoComplete
@@ -177,7 +185,11 @@ export default function BuildImage() {
               options={options}
               disabled={blockBuildButton}
             >
-              <Input.Search size='large' placeholder='' disabled={blockBuildButton} />
+              <Input.Search
+                size='large'
+                placeholder=''
+                disabled={blockBuildButton}
+              />
             </AutoComplete>
           </Form.Item>
           <Row gutter={8}>
