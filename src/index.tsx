29cb0d58327eb456d7333d4e99ba8499237dfd4a
Aaron Huang <aaroms9733@gmail.com>
2021-10-02 10:40:13 +0800

    Clean script
    Signed-off-by: Aaron Huang <aaroms9733@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -7,9 +7,7 @@ import * as ipcClient from './utils/ipcClient';
 ipcClient.init();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
